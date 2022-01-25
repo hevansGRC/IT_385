7fbf7e62aa7fc7c73d13d1309099d93481b04cea
Hunter Evans <hunter@evansnetwerkes.com>
2022-01-24 21:41:13 -0700

    WIP

--- Python/RandomPassGenerator.py
+++ Python/RandomPassGenerator.py
@@ -19,19 +19,79 @@ upper_case = string.ascii_uppercase
 num = string.digits
 symbols = string.punctuation
 
-passwd_var = lower_case + upper_case + num + symbols
+default_passwd_var = lower_case + upper_case + num + symbols
 
 # Input passwd length function
 def passwd_length():
     passwd_length = int(input("How many characters in your password human: "))
-    
     return passwd_length
 user_passwd_length = passwd_length()
 
+# Input desired character set
+def passwd_var_set():
+    def choose_symbols():
+        user_symbols = input("Would you like to use symbols and punctuation (yes/no): ")
+        if user_symbols == "yes":
+            return symbols
+        elif user_symbols == "no":
+            pass
+        else:
+            print("Please type either 'yes' or 'no'")
+            choose_symbols()
+    user_symbols = choose_symbols()
+    def choose_numbers():
+        user_num = input("Would you like to use numbers (yes/no): ")
+        if user_num == "yes":
+            return num
+        elif user_num == "no":
+            pass 
+        else:
+            print("Please type either 'yes' or 'no'")
+            choose_numbers()
+    user_num = choose_numbers()
+    def choose_uppercase():
+        user_upper_case = input("Would you like to use upper case letters (yes/no): ")
+        if user_upper_case == "yes":
+            return upper_case
+        elif user_upper_case == "no":
+            pass
+        else:
+            print("Please type either 'yes' or 'no'")
+            choose_uppercase()
+    user_upper_case = choose_uppercase()
+    def choose_lowercase():
+        user_lower_case = input("Would you like to use lower case case letters (yes/no): ")
+        if user_lower_case == "yes":
+            return lower_case
+        elif user_lower_case == "no":
+            pass 
+        else:
+            print("Please type either 'yes' or 'no'")
+            choose_lowercase()
+    user_lower_case = choose_lowercase()
+    def pass_var_list():
+        var_list = []
+        if bool(user_symbols) == True:
+            var_list.append(user_symbols)
+        if bool(user_num) == True:
+            var_list.append(user_num)
+        if bool(user_lower_case) == True:
+            var_list.append(user_lower_case)
+        if bool(user_upper_case) == True:
+            var_list.append(user_upper_case)
+        return var_list
+    def add_pass_var_list():
+        return a + b
+    user_passwd_var = map(add_pass_var_list, )
+user_passwd_var = passwd_var_set()
+
 # Generate passwd function
 def passwd_gen():
-    passwd = ''.join(secrets.choice(passwd_var) for i in range(user_passwd_length)) 
-    return passwd
+    if user_passwd_length == default_passwd_var:
+        passwd = ''.join(secrets.choice(default_passwd_var) for i in range(user_passwd_length)) 
+        return passwd
+    elif user_passwd_length != default_passwd_var:
+        passwd = ''.join(secrets.choice(user_passwd_var) for i in range(user_passwd_length))
 passwd = passwd_gen()
 
 # Print passwd
