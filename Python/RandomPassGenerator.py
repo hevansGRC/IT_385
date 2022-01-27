59dbfc2f2ceccf8e9792bf7df3bd77f26cdcf4e5
Hunter Evans <hunter@evansnetwerkes.com>
2022-01-26 20:06:06 -0700

    Rev_3

--- Python/RandomPassGenerator.py
+++ Python/RandomPassGenerator.py
@@ -46,40 +46,41 @@ def default_passwd():
 def choose_symbols():
     user_choice = input("Would you like to use symbols and punctuation (yes/no): ")
     if user_choice == "yes":
-        user_passwd_var = symbols
-        return user_passwd_var
-    elif "no":
+        user_var = string.punctuation
+        return user_var
+    elif user_choice == "no":
         pass
     else:
         print("Please type either 'yes' or 'no'")
         choose_symbols()
 
 def choose_numbers():
-        user_choice = input("Would you like to use numbers (yes/no): ")
-        if user_choice == "yes":
-            user_passwd_var = user_passwd_var + num
-            return user_passwd_var
-        elif user_num == "no":
-            pass 
-        else:
-            print("Please type either 'yes' or 'no'")
-            choose_numbers()
+    user_choice = input("Would you like to use numbers (yes/no): ")
+    if user_choice == "yes":
+        user_var = string.digits
+        return user_var
+    elif user_choice == "no":
+        pass 
+    else:
+        print("Please type either 'yes' or 'no'")
+        choose_numbers()
 
 def choose_uppercase():
-        user_choice = input("Would you like to use upper case letters (yes/no): ")
-        if user_choice == "yes":
-            user_passwd_var = user_passwd_var + upper_case
-            return user_passwd_var
-        elif user_choice == "no":
-            pass
-        else:
-            print("Please type either 'yes' or 'no'")
-            choose_uppercase()
+    user_choice = input("Would you like to use upper case letters (yes/no): ")
+    if user_choice == "yes":
+        user_var = string.ascii_uppercase
+        return user_var
+    elif user_choice == "no":
+        pass
+    else:
+        print("Please type either 'yes' or 'no'")
+        choose_uppercase()
 
 def choose_lowercase():
     user_choice = input("Would you like to use lower case case letters (yes/no): ")
     if user_choice == "yes":
-        return lower_case
+        user_var = string.ascii_lowercase
+        return user_var
     elif user_choice == "no":
         pass 
     else:
@@ -90,9 +91,9 @@ def choose_lowercase():
 user_passwd_length = passwd_length()
 default_passwd()
 user_passwd_var = choose_symbols()
-user_passwd_var = choose_numbers()
-user_passwd_var = choose_uppercase()
-user_passwd_var = choose_lowercase()
+user_passwd_var = choose_numbers() + user_passwd_var
+user_passwd_var = choose_uppercase() + user_passwd_var
+user_passwd_var = choose_lowercase() + user_passwd_var
 
 #user_settings = var_set.translate({ord('):None})
 # Generate user_passwd function
