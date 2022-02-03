881da275874e116df744e9991f2779611753eefc
Hunter Evans <hunter@evansnetwerkes.com>
2022-02-02 18:50:35 -0700

    Rev_4

--- Python/RandomPassGenerator.py
+++ Python/RandomPassGenerator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 # Week 4 - Random Password Generator
 # allow the user to choose different lengths (4-300 characters)
 # allow the user to select different types of characters (symbols, numbers, lower case, upper case)
@@ -8,7 +8,7 @@
 # allow the user to ensure the first character is a letter
 # allow the user to generate multiple passwords to choose from
 # color-code letters, numbers, and symbols for easy identification
-
+#!/usr/bin/env python3
 # Import modules
 import string
 import secrets
@@ -28,78 +28,14 @@ def default_passwd_gen():
 
 # Input passwd length function
 def passwd_length():
-    passwd_length = int(input("How many characters in your password human: "))
-    return passwd_length
-
-# Input default or user defined settings
-def default_passwd():
-    user_setting = input("Would you like to use the default character settings (yes/no): ")
-    if user_setting == "yes":
-        default_passwd_gen()
-    elif user_setting == "no":
+    global user_passwd_length
+    user_passwd_length = int(input("How many characters in your password human: "))
+    if user_passwd_length >= 4 and user_passwd_length <= 300:
         pass
+
     else:
-        print("Please type either 'yes' or 'no'")
-        default_passwd()
+        print("Please choose a number between 4 and 300 human...")
+        passwd_length()
 
-# Input desired character var
-#def choose_symbols():
-    user_choice = input("Would you like to use symbols and punctuation (yes/no): ")
-    if user_choice == "yes":
-        user_symbols = string.punctuation
-        return user_symbols
-    elif user_choice == "no":
-        pass
-    else:
-        print("Please type either 'yes' or 'no'")
-        choose_symbols()
-
-#def choose_numbers():
-    user_choice = input("Would you like to use numbers (yes/no): ")
-    if user_choice == "yes":
-        user_num = string.digits
-        return user_num
-    elif user_choice == "no":
-        pass 
-    else:
-        print("Please type either 'yes' or 'no'")
-        choose_numbers()
-
-#def choose_uppercase():
-    user_choice = input("Would you like to use upper case letters (yes/no): ")
-    if user_choice == "yes":
-        user_uppercase = string.ascii_uppercase
-        return user_uppercase
-    elif user_choice == "no":
-        pass
-    else:
-        print("Please type either 'yes' or 'no'")
-        choose_uppercase()
-
-#def choose_lowercase():
-    user_choice = input("Would you like to use lower case case letters (yes/no): ")
-    if user_choice == "yes":
-        user_lowercase = string.ascii_lowercase
-        return user_lowercase
-    elif user_choice == "no":
-        pass
-    else:
-        print("Please type either 'yes' or 'no'")
-        choose_lowercase()
-
-# Set passwd variables
-user_passwd_length = passwd_length()
-default_passwd()
-user_symbols = choose_symbols()
-user_num = choose_numbers()
-user_uppercase = choose_uppercase()
-user_lowercase = choose_lowercase()
-#user_passwd_var = add_user_var()
-
-#user_settings = var_set.translate({ord('):None})
-# Generate user_passwd function
-def user_passwd_gen():
-    passwd = ''.join(secrets.choice(user_passwd_var) for i in range(user_passwd_length))
-    print("Human, I present your password: {0}".format(passwd))
-    raise SystemExit
-user_passwd_gen()
+passwd_length()
+default_passwd_gen()
